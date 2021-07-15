a80ccf19743c04739f83227df4b9143a85a51efc
Hasuto27 <harysk01@gmail.com>
2021-07-15 21:09:08 +0700

    Homework 14 July

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import Playlist from './component.js';
-import data from './data.js'
+import Playlist from './component/component.js';
+import data from './data.js';
 function App() {
   return (
       <div className="App">
           <div class="playlist-container">
-              <Playlist/>
+              <Playlist
+                  title={data.name}
+                  artist={data.artists[0].name}
+                  album={data.album.name}
+                  imgurl={data.album.images[0].url}
+                  songurl={data.album.artists[0].external_urls.spotify}
+              />
           </div>
     </div>
   );
--- src/component/component.js
+++ src/component/component.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReachDOM from 'react-dom';
 
+
 class Playlist extends React.Component {
     render() {
         return (
             <div class="playlist-item divstyle">
                 <div class="playlist-item-left">
-                    <img src="https://i.scdn.co/image/ab67616d00004851e8b066f70c206551210d902b" alt="Alternate Text" width="100px" height="100px" />
+                    <img src={this.props.imgurl} alt="Alternate Text" width="100px" height="100px" />
                 </div>
 
                 <div class="playlist-item-right">
-                    <h3>Bohemian Rhapsody (The Original Soundtrack)</h3>
-                    <p>Queen</p>
-                    <button class="button button-playlist">Select</button>
+                    <h3>{this.props.title}</h3>
+                    <p>{this.props.artist}</p>
+                    <p>{this.props.album}</p>
+                    <a class="button button-playlist" href={ this.props.songurl}>Select</a>
                 </div>
             </div>
             );
